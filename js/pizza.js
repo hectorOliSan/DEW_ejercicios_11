719aedf524719c20319b763a3b67e09fce83c620
Héctor Olivares Sánchez <hectorolivares35@gmail.com>
2023-02-14 09:28:26 +0000

    Ejercicios Unidad 11

--- js/pizza.js
+++ js/pizza.js
@@ -38,23 +38,32 @@ document.getElementById("Regular").checked = true;
 const pizza = document.getElementById("pizza");
 const prep_pizza = document.getElementById("prep_pizza");
 prep_pizza.addEventListener("click", () => {
+  pizza.innerHTML = "";
+
   const checkedToppings = Array.from(
     document.querySelectorAll('input[name="toppings"]:checked')
   ).map((t) => t.parentNode.nextSibling.nextSibling.innerText);
-  console.log(checkedToppings);
 
   const selectedCrust = Array.from(
     document.querySelectorAll('input[name="crusts"]:checked')
   ).map((c) => c.parentNode.nextSibling.nextSibling.innerText);
 
-  pizza.innerText = "You must select toppings for the pizza";
   if (checkedToppings.length > 0) {
-    pizza.innerHTML = `
-    This pizza will have:
-    <br>
-    ${checkedToppings.join("<br>")}
-    <br>
-    ${selectedCrust[0]} Crust
-  `;
+    crearNodoTexto("This pizza will have:");
+
+    checkedToppings.forEach((t) => {
+      crearNodoTexto(t);
+    });
+
+    crearNodoTexto(`${selectedCrust[0]} Crust`);
+
+  } else {
+    crearNodoTexto("You must select toppings for the pizza");
   }
 });
+
+const crearNodoTexto = (texto) => {
+  const p = document.createElement("p");
+  p.appendChild(document.createTextNode(texto));
+  pizza.appendChild(p);
+};
